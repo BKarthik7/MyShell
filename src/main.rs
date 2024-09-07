43bbcd8575d20f6d91c806c979d57e784a58239e
B Karthik <bangikarthik7@gmail.com>
2024-09-07 20:31:53 +0530

    Starting Point Input taking and triming

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::Command;
+
+use std::io::stdin;
+
+fn main(){
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+
+    let command = input.trim();
+
+    Command::new(command)
+        .spawn()
+        .unwrap();
 }
